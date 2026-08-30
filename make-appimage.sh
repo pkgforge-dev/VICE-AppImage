5b6d1faf6277eea2d19cc8b1359f87ed61e2347e
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:31:54 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,8 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/VICE-Team/svn-mirror/e635822a932aba8387fabb454d4d59218a952187/vice/data/common/CBM_Logo.svg
 export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/c1541 \
