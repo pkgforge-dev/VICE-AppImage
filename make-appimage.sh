4ff3aaf25e478ee2d28dff2ca10377d641baa5bb
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:32:11 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -30,7 +30,3 @@ quick-sharun /usr/bin/c1541 \
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
